14aa07bad00e6ec09d40b31f9641b8d9fda7b641
maome <steelereilly@gmail.com>
2013-07-12 10:22:17 -0700

    comments about request_handler.rb

--- request_handler.rb
+++ request_handler.rb
@@ -1,3 +1,13 @@
+#my vision for request handler sits between the sinatra api server and
+#the wrappers for any given ranking system
+#
+#as of now the ranking system could be changed by replacing the wrapper
+#file with an appropriate resource for a different ranking system
+#I would like to improve this so the files could be automatically chosen
+#perhaps this will be done by using a require 'variable' which can be
+#changed during runtime but I'm not sure enough about ruby to even say
+#if that is a possibility
+
 def reportGameResults(user, winner, loser, scores_and_stuff?)
     #if user != winner and user != loser reject!
     #set up db to ask other player to respond to results
