f6f42cb727c0a32c39fc4d8170d80f19881f3cd6
maome <steelereilly@gmail.com>
2013-07-12 10:19:28 -0700

    added skeleton for request handler

---
+++ request_handler.rb
@@ -0,0 +1,24 @@
+def reportGameResults(user, winner, loser, scores_and_stuff?)
+    #if user != winner and user != loser reject!
+    #set up db to ask other player to respond to results
+end
+
+def getGameResults(gameID)
+    #return all info from game in json?
+end
+
+def login(user, password)
+    #this maybe should error on fail
+    #else return a login package that includes getUpdates?
+end
+
+def getUpdates(user)
+    #send a package with any information the user should know about
+    #right now I'm just thinking, games that need approval from them
+end
+
+def acceptGame(user, gameID)
+    #move the game from accepted to game history table, make sure it belongs to the user
+end
+
+
