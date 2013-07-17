88a534879f0fcc83d59382c26508f40d8b4440f0
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2013-07-16 21:49:13 -0700

    Improved comments and readability.

--- server/api_server.rb
+++ server/api_server.rb
@@ -3,9 +3,10 @@
 require 'sinatra'
 require_relative 'request_handler'
 
+#Enable use of sessions for easy logging/state management
 enable :sessions
-#should enable us to handle logging in easily
 
+#Verify login information
 def checkLogin()
     #p "userid = " + session['userID'].to_s
     if session['user_id'] > 0
@@ -14,13 +15,16 @@ def checkLogin()
     return false
 end
 
+#Set up port and hostname for Sinatra
 set :port => 10081
 set :bind => "0.0.0.0"
 
+#Hello, world
 get '/' do
-    "hello!"
+    "Rhubarb says hello!"
 end
 
+#Authentication
 get '/auth' do
     u = params[:username]
     p = params[:password]
@@ -33,13 +37,18 @@ get '/auth' do
     end
 end
 
+#Run checkLogin() to test authentication
 get '/testAuth' do
     checkLogin().to_s
 end
 
+#Forward new game requests to the request handler
 get '/addNewGame' do
+
+    #If we don't have a valid login display error
     if !checkLogin()
         "Invalid login"
+    #Otherwise get request data and forward to appropriate handling method
     else
         action = params[:action]
         winner_id = params[:winner_id]
@@ -50,14 +59,16 @@ get '/addNewGame' do
         recipient_id = params[:recipient_id]
         pending_game_id = params[:pending_game_id]
 
-        #somecomment
+        #If action is new request a new addition to the pending table
         if (action == "new")
             gameRequest(action, winner_id, loser_id, high_score, low_score, player_id, recipient_id)
+        #If action is accept then accept an existing game, move it to games and remove from pending games
         elsif (action == "accept")
             gameAccept(player_id, pending_game_id)
-            p player_id
+        #If action is decline then decline an existing game, remove it from pending games
         elsif (action == "decline")
             gameDecline(player_id, pending_game_id)
+        #If action is cancel then cancel an existing game, remove it from pending games
         elsif (action == "cancel")
             gameCancel(player_id, pending_game_id)
         else
@@ -65,17 +76,20 @@ get '/addNewGame' do
 
     end
 end
-    
+
+#Forward requests for game info to the request handler
 get '/getGameInfo' do
     gid = params[:game_id]
     getGameInfo(gid)
 end
 
+#Forward requests for player info to the request handler
 get '/getPlayerInfo' do
     pid = params[:player_id]
     getPlayerInfo(pid)
 end
 
+#Forward requests for games to the request handler
 get '/getGames' do
     getTopGames(-1)
 end
--- server/request_handler.rb
+++ server/request_handler.rb
@@ -5,7 +5,7 @@ require_relative('mysql_connection')
 #Initiate a connection to the database defined by mysql_connection.rb
 $con = Mysql.new($hostname, $username, $password, $database)
 
-#COMMENT-REQUIRED
+#Authenticate a user by checking the supplied password and username against the database
 def auth(username, password)
     results = $con.query("select id, login from rhubarb_players where login='" + username +"' AND password='" + password + "';")
     if results.num_rows() == 1
@@ -16,7 +16,7 @@ def auth(username, password)
     end
 end
 
-#COMMENT-REQUIRED
+#Get a JSON-formatted player info for a particular player_id
 def getPlayerInfo(player_id)
     results = $con.query("select name, login, rating_package, rating from rhubarb_players where id=" + player_id.to_s + ";")
     if results.num_rows() == 0
@@ -26,7 +26,7 @@ def getPlayerInfo(player_id)
     return JSON.fast_generate(row)
 end
 
-#COMMENT-REQUIRED
+#Get a JSON-formatted list of players by name or ranking
 def getPlayers(name, low, high)
     if name == ""
         #then do high low
@@ -40,7 +40,7 @@ def getPlayers(name, low, high)
     end
 end
 
-#COMMENT-REQUIRED
+#Get a JSON-formatted string containing info for a particular game_id
 def getGameInfo(game_id)
     results = $con.query("select * from rhubarb_games where id=" + game_id.to_s + ";")
     if results.num_rows() == 0
@@ -50,7 +50,7 @@ def getGameInfo(game_id)
     return JSON.fast_generate(row)
 end
 
-#COMMENT-REQUIRED
+#Get JSON-formatted game info for games between name1 and name2
 def getGamesByPlayers(name1, name2)
     results = $con.query("select * from rhubarb_games where (winner=" + name1.to_s + " and loser=" + name2.to_s + ") or (winner=" + name2.to_s + " and loser=" + name1.to_s + ");")
     rows = Array.new
@@ -60,7 +60,8 @@ def getGamesByPlayers(name1, name2)
     return JSON.fast_generate(rows)
 end
 
-#COMMENT-REQUIRED
+#Get top games? Haven't looked this function over yet.
+#COMMENT-FIX-REQUIRED
 def getTopGames(top)
     if top == -1
         results = $con.query("select * from rhubarb_games order by id desc limit 50")
@@ -74,7 +75,7 @@ def getTopGames(top)
     return JSON.fast_generate(rows)
 end
 
-#COMMENT-REQUIRED
+#Get a list of pending games for player_id and return corresponding JSON
 def getPendingGames(player_id)
     results = $con.query("select * from rhubarb_pending_games where requested_to=" + player_id.to_s + ";")
     rows = Array.new
@@ -84,14 +85,14 @@ def getPendingGames(player_id)
     return JSON.fast_generate(rows)
 end
 
-#COMMENT-REQUIRED
-#ERROR-HANDLING-REQUIRED
+#Request a game by adding the necessary information to pending games in the database
+#ERROR-HANDLING-NEEDED
 def gameRequest(winner, loser, high_score, low_score, player_id, recipient_id)
     results = $con.query("insert into rhubarb_pending_games (requested_by, requested_to, timestamp, winner, loser, high_score, low_score) values (" + player_id.to_s + "," + recipient_id.to_s + "," + "NOW() ," + winner.to_s + "," + loser.to_s + "," + high_score.to_s + "," + low_score.to_s + ");")
     return 0
 end
 
-#COMMENT-REQUIRED
+#Accept a game by moving info in the database from pending games to games provided the request was sent to player_id
 def gameAccept(player_id, pending_game_id)
     results = $con.query("select * from rhubarb_pending_games where requested_to=" + player_id.to_s + " and id=" + pending_game_id.to_s+ ";")
     row = results.fetch_hash
@@ -99,21 +100,12 @@ def gameAccept(player_id, pending_game_id)
     $con.query("delete from rhubarb_pending_games where id=" + pending_game_id.to_s + " and requested_to=" + player_id.to_s + ";")
 end
 
-#COMMENT-REQUIRED
+#Cancel a game by removing from the database the game matching pending_game_id if it was also requested by player_id
 def gameCancel(player_id, pending_game_id)
+    $con.query("delete from rhubarb_pending_games where id=" + pending_game_id.to_s + " and requested_by=" + player_id.to_s + ";")
 end
 
-#COMMENT-REQUIRED
+#Decline a game by removing from the database the game matching pending_game_id if it was also sent to player_id
 def gameDecline(player_id, pending_game_id)
     $con.query("delete from rhubarb_pending_games where id=" + pending_game_id.to_s + " and requested_to=" + player_id.to_s + ";")
 end
-
-#TESTING FUNCTIONS
-
-#p auth("reilly", "password")
-#p getPlayerInfo(0)
-#p getPlayers("", 0.0, 5, 0)
-#p getGames(0, 1)
-#p getPendingGames(0)
-#p gameRequest("new", 0, 1, 11, 6, 1, 0)
-#p getPendingGames(0)
