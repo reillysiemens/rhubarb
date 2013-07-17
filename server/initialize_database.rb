994f84de33c219e624ec6616d78b056cab0ecd78
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2013-07-17 01:48:19 -0700

    Further updates to DB schema: Changed instances of high_score and low_score to winner_score and loser_score. Changed requested_by to sender_id. Changed requested_to to recipient_id.

--- server/initialize_database.rb
+++ server/initialize_database.rb
@@ -16,10 +16,10 @@ p "Attempting to create tables in database '" + opts[:db_database] +"' at host '
 File.open('db_connection.rb','w') { |file| file.write("$hostname = \"" + opts[:db_location].to_s + "\"\n$database = \"" + opts[:db_database].to_s + "\"\n$username = \"" + opts[:db_user].to_s + "\"\n$password = \"" + opts[:db_password].to_s + "\"")} 
 
 #THIS
-create_rhubarb_games = "CREATE TABLE rhubarb_games (id int(11) NOT NULL AUTO_INCREMENT,winner int(11) NOT NULL,loser int(11) NOT NULL,high_score int(11) NOT NULL,low_score int(11) NOT NULL,timestamp time NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;"
+create_rhubarb_games = "CREATE TABLE rhubarb_games (id int(11) NOT NULL AUTO_INCREMENT,winner int(11) NOT NULL,loser int(11) NOT NULL,winner_score int(11) NOT NULL,loser_score int(11) NOT NULL,timestamp time NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;"
 
 #IS
-create_rhubarb_pending_games = "CREATE TABLE rhubarb_pending_games (id int(11) NOT NULL AUTO_INCREMENT,requested_by int(11) NOT NULL,requested_to int(11) NOT NULL,winner int(11) NOT NULL,loser int(11) NOT NULL,high_score int(11) NOT NULL,low_score int(11) NOT NULL,timestamp time NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;"
+create_rhubarb_pending_games = "CREATE TABLE rhubarb_pending_games (id int(11) NOT NULL AUTO_INCREMENT,sender_id int(11) NOT NULL,recipient_id int(11) NOT NULL,winner int(11) NOT NULL,loser int(11) NOT NULL,winner_score int(11) NOT NULL,loser_score int(11) NOT NULL,timestamp time NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;"
 
 #UGLY
 create_rhubarb_players = "CREATE TABLE rhubarb_players (id int(11) NOT NULL,name varchar(50) NOT NULL,username varchar(50) NOT NULL,password varchar(200) NOT NULL,rating_package varchar(200) DEFAULT NULL,rating double DEFAULT NULL,PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;" 
