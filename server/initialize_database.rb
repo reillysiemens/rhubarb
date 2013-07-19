0e0d6ef46fd9cb0549433a4ff7c00579b721af81
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2013-07-19 16:30:00 -0700

    Modified DB schema, merged rhubarb_pending_games with rhubarb_games and added rhubarb_ratings table.

--- server/initialize_database.rb
+++ server/initialize_database.rb
@@ -16,13 +16,11 @@ p "Attempting to create tables in database '" + opts[:db_database] +"' at host '
 File.open('db_connection.rb','w') { |file| file.write("$hostname = \"" + opts[:db_location].to_s + "\"\n$database = \"" + opts[:db_database].to_s + "\"\n$username = \"" + opts[:db_user].to_s + "\"\n$password = \"" + opts[:db_password].to_s + "\"")} 
 
 #THIS
-create_rhubarb_games = "CREATE TABLE rhubarb_games (id int(11) NOT NULL AUTO_INCREMENT,winner int(11) NOT NULL,loser int(11) NOT NULL,winner_score int(11) NOT NULL,loser_score int(11) NOT NULL,timestamp time NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;"
-
-#IS
-create_rhubarb_pending_games = "CREATE TABLE rhubarb_pending_games (id int(11) NOT NULL AUTO_INCREMENT,sender_id int(11) NOT NULL,recipient_id int(11) NOT NULL,winner int(11) NOT NULL,loser int(11) NOT NULL,winner_score int(11) NOT NULL,loser_score int(11) NOT NULL,timestamp time NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;"
-
+create_rhubarb_games = "CREATE  TABLE `rhubarb_games` (`id` INT NOT NULL AUTO_INCREMENT ,`winner` INT NOT NULL ,`loser` INT NOT NULL ,`winner_score` INT NOT NULL ,`loser_score` INT NOT NULL ,`sender_id` INT NOT NULL ,`recipient_id` INT NOT NULL ,`timestamp` INT NOT NULL ,`status` INT NOT NULL ,PRIMARY KEY (`id`) )ENGINE = InnoDB;"
 #UGLY
-create_rhubarb_players = "CREATE TABLE rhubarb_players (id int(11) NOT NULL,name varchar(50) NOT NULL,username varchar(50) NOT NULL,password varchar(200) NOT NULL,rating_package varchar(200) DEFAULT NULL,rating double DEFAULT NULL,PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;" 
+create_rhubarb_players = "CREATE  TABLE `rhubarb_players` (`id` INT NOT NULL AUTO_INCREMENT ,`name` VARCHAR(70) NOT NULL ,`username` VARCHAR(20) NOT NULL ,`password` VARCHAR(64) NOT NULL ,`rating_package` VARCHAR(64) NOT NULL ,`current_rating` INT NOT NULL ,PRIMARY KEY (`id`) )ENGINE = InnoDB;"
+
+create_rhubarb_ratings = "CREATE  TABLE `rhubarb_ratings` (`id` INT NOT NULL AUTO_INCREMENT ,`player_id` INT NOT NULL ,`game_id` INT NOT NULL ,`rating_delta` INT NOT NULL ,`rating` INT NOT NULL ,PRIMARY KEY (`id`) )ENGINE = InnoDB;"
 
 #Create a connection to the database with the supplied information
 con = Mysql.new(opts[:db_location], opts[:db_user], opts[:db_password], opts[:db_database])
@@ -30,7 +28,7 @@ con = Mysql.new(opts[:db_location], opts[:db_user], opts[:db_password], opts[:db
 #Create tables in database
 con.query(create_rhubarb_games)
 p "Successfully created table rhubarb_games in database '" + opts[:db_database] + "'."
-con.query(create_rhubarb_pending_games)
-p "Successfully created table rhubarb_pending_games in database '" + opts[:db_database] + "'."
 con.query(create_rhubarb_players)
 p "Successfully created table rhubarb_players in database '" + opts[:db_database] + "'."
+con.query(create_rhubarb_ratings)
+p "Successfully created table rhubarb_ratings in database '" + opts[:db_database] + "'."
